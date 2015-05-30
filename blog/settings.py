dfe3d8a6b22849d4f9c080a3197b7fb31a0b8879
Tushar <tkg.1996@gmail.com>
2015-05-30 11:04:58 +0530

    Yo

--- blog/settings.py
+++ blog/settings.py
@@ -59,11 +59,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gargnotes',
-        'USER': 'root',
-        'PASSWORD' : 'root',
-        'HOST' : '/Applications/MAMP/tmp/mysql/mysql.sock',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
