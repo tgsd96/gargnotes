2e1a17ed34de4faa5268a6252d933e420a3ef296
Tushar <tkg.1996@gmail.com>
2015-05-30 15:26:02 +0530

    final effort

--- blog/settings.py
+++ blog/settings.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https'
 
 
 # Quick-start development settings - unsuitable for production
@@ -60,7 +64,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME' : os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
